b2f650df99ccb08e5f2dd057c12c08c113828cbe
nonononoka <onon.kaori@gmail.com>
2024-03-01 23:35:33 +0900

    change index

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ require("dotenv").config();
 const { ApolloServer } = require("apollo-server");
 const typeDefs = require("./schema");
 const resolvers = require("./resolvers/index.js");
-console.log(resolvers)
+console.log(resolvers);
 const server = new ApolloServer({ typeDefs, resolvers });
 
 server.listen().then(({ url }) => {
-  console.log(`🚀 Server ready at ${url}`);
+  console.log(`🚀 Server ready  ${url}`);
 });
